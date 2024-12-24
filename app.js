70ffe3f9c23b61b3a9279d85ad7ba00349f0eb93
hykals2 <220016110webmail.uad.ac.id>
2024-12-24 23:38:41 +0700

    prak8

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.get("/query", (req, res) => {
+  const name = req.query.name || "Guest";
+  res.send(`Hello, ${name}`);
+});
+
+app.get("/json", (req, res) => {
+  res.json({ message: "This is a JSON response" });
+});
+
+app.use((req, res) => {
+  res.status(404).send("Not Found");
+});
+
 module.exports = app;
